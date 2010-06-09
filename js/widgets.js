288f9ce9bf6cf1baa1c4ce6176f31db510e0f0a5
Richard Willis <willis.rh@gmail.com>
2010-06-09 21:53:11 +0100

    moving files, added widgets file

--- js/widgets.js
+++ js/widgets.js
@@ -185,7 +185,88 @@ destroy:function(){this.element.removeClass("ui-buttonset");this.buttons.map(fun
  * @Author Richard Willis
  *
  */
- (function(c){c.fn.render=function(b){function d(a){this.nodeType===3&&a&&a.call(this);c.each(this.childNodes,function(){d.call(this,a)})}function e(a){this.nodeValue=a}function f(a){c(this).after(a).remove()}b=b||{};return this.each(function(){d.call(this,function(){for(var a=this.nodeValue.replace(new RegExp("\\n","g"),"");match=/\{(.*?)\}/g.exec(a);)if(b[match[1]]){var g=/<[^>]+>/.test(b[match[1]])?f:e,h=this.nodeValue.replace(new RegExp("\\${"+match[1]+"}","g"),b[match[1]]);g.call(this,h)}})})}})(jQuery);
+
+(function( $ ) {
+
+	$.fn.render = function(data, options){
+
+		data = data || {};
+
+		options = $.extend({
+			attrs: 'href,class,src'
+		}, options);
+
+		function walkTextNodes(textNodeCallback, elementNodeCallback){
+
+			( this.nodeType === 3 && textNodeCallback ) && textNodeCallback.call( this );
+
+			( this.nodeType === 1 && elementNodeCallback ) && elementNodeCallback.call( this );
+
+			$.each(this.childNodes, function(){
+
+				walkTextNodes.call(this, textNodeCallback, elementNodeCallback);
+			});
+		}
+
+		function replaceText( val ){
+
+			this.nodeValue = val;
+		}
+
+		function replaceHTML( val ){
+				
+			$( this ).after( val ).remove(); 
+		}
+
+		function replace( val ) {
+
+			while( match = /\{(.*?)\}/g.exec( val ) ) {
+
+				if ( data[match[1]] ) { 
+			
+					func = /<[^>]+>/.test( data[match[1]] ) ? replaceHTML : replaceText,
+
+					val = val.replace( new RegExp('\\$\{' + match[1] + '\}', 'g'), data[match[1]] );
+
+					func.call( this, val );
+				}
+			}
+		}
+
+		return this.each(function(){
+
+			walkTextNodes.call(this, 
+			function(){
+				// textnode callback
+	
+				var nodeValue = this.nodeValue.replace( new RegExp('\\n', 'g'), '' );
+
+				replace.call( this, nodeValue );
+
+			},
+			function(){
+				// elementnode callback
+
+				var el = this;
+
+				$.each(options.attrs.split(','), function(){
+
+					var attr = el[this];
+
+					if ( attr && typeof attr == 'string' ){
+
+						var text = document.createTextNode( attr );	
+
+						replace.call( text, unescape(attr) );
+
+						el[this] = text.nodeValue;
+					}
+				});
+			});
+		});
+	}; 
+
+})( jQuery );
 
 /*
  * jQuery UI Scrollbar
